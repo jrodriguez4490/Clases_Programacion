b4b066db8da0daebc0d39cfeb4d9ca9030ecc952
jrodriguez4490 <jrodriguez4490@ceuandalucia.es>
2023-11-29 14:17:52 +0100

    ejercicio3

---
+++ Programacion/src/ejerciciosTemaCuatro/E3.java
@@ -0,0 +1,32 @@
+package ejerciciosTemaCuatro;
+
+import java.util.Scanner;
+
+public class E3 {
+public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	String respuesta = getMayMin(sc, null);
+	System.out.println(respuesta);
+}
+public static String getMayMin(Scanner respuesta,String texto) {
+	
+	System.out.println("Dame un número. El 1 o el 2.");
+	String numero = respuesta.nextLine();
+	String cadena = "";
+	System.out.println("Dame una cadena");
+
+	if (numero.equals("1")) {
+		 cadena = respuesta.nextLine();
+		 cadena = cadena.trim().toUpperCase();
+	} else if (numero.equals("2")) {
+		cadena=respuesta.nextLine();
+		cadena = cadena.trim().toLowerCase();
+		
+	}
+	
+	return cadena;
+	
+	
+}
+}
+
